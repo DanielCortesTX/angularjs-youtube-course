fb1356102384e766194c3d617b1ce052a5eeda19
unknown <dcorteswebdev@gmail.com>
2019-10-10 16:57:21 -0500

    feat: Filter include and show/hide

--- app/app.js
+++ app/app.js
@@ -3,7 +3,32 @@ var myNinjaApp = angular.module('myNinjaApp', []);
 myNinjaApp.controller('NinjaController', ['$scope', function($scope){
   $scope.message = "Hey Y'all"
 
-  $scope.ninjas = ['yoshi', 'crystal', 'ryu', 'shaun']
+  $scope.ninjas = [
+    {
+      name: 'yoshi',
+      belt: 'green',
+      rate: 50,
+      available: true
+    },
+    {
+      name: 'crystal',
+      belt: 'yellow',
+      rate: 30,
+      available: true
+    },
+    {
+      name: 'Ryu',
+      belt: 'orange',
+      rate: 10,
+      available: false
+    },
+    {
+      name: 'shaun',
+      belt: 'black',
+      rate: 1000,
+      available: true
+    }
+  ]
 }])
 
 // myNinjaApp.config(function(){
